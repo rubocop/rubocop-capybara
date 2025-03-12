605ee6bf04a486a9adc074ccab8f85c4d5a8049c
Benjamin Quorning <benjamin@quorning.net>
2025-03-12 17:01:14 +0100

    Release v2.22.1

--- lib/rubocop/capybara/version.rb
+++ lib/rubocop/capybara/version.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Capybara
     # Version information for the Capybara RuboCop plugin.
     module Version
-      STRING = '2.22.0'
+      STRING = '2.22.1'
     end
   end
 end
