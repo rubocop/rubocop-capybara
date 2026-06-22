6a4579d93be9d23c261ccf06143c0c8d4fefddd3
ydah <t.yudai92@gmail.com>
2026-06-22 20:56:25 +0900

    release v3.0.0

--- lib/rubocop/capybara/version.rb
+++ lib/rubocop/capybara/version.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Capybara
     # Version information for the Capybara RuboCop plugin.
     module Version
-      STRING = '2.23.0'
+      STRING = '3.0.0'
     end
   end
 end
